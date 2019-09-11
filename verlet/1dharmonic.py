78600bca9850a69f68b83a76661bb0d9131e62a5
Alan Robledo <ar6138@nyu.edu>
2019-09-10 20:49:06 -0400

    started verlet algorithm code

---
+++ verlet/1dharmonic.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_force(position): # F = - grad(U) = - grad(.5 k x^2) = - kx = - m w^2 x
+    return - m * w**2 * position
+
+tmin = 0        # Starting time
+dt = 0.01       # Time step
+n_steps = 3000  # Number of time steps
+
+times = [tmin + i * dt for i in range(ngrid)]
+x = []      # Positions
+v = []      # Velocities
+f = []      # Forces
+x.append(0) # Initial position
+v.append(1) # Initial velocity
+m = 1       # Mass
+w = 1       # Angular Frequency for spring
+f.append(get_force(x[0])) # Compute inital force
+
+for i in range(ngrid):
+    x.append( x[i] + v[i] * dt + ((f[i])/(2 * m) * dt**2) )
+    v_half = v[i] + (f[i]/(2 * m)) * dt
+    if i < ngrid:
+        f.append(get_force(x[i + 1]))
+    v.append( v_half + (f[i + 1]/(2 * m)) * dt )
