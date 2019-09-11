638994a4bd5d940db7db3afb8f07f2ca00643051
Alan Robledo <ar6138@nyu.edu>
2019-09-10 20:50:26 -0400

    changed variable names

--- verlet/1dharmonic.py
+++ verlet/1dharmonic.py
@@ -8,7 +8,7 @@ tmin = 0        # Starting time
 dt = 0.01       # Time step
 n_steps = 3000  # Number of time steps
 
-times = [tmin + i * dt for i in range(ngrid)]
+times = [tmin + i * dt for i in range(n_steps)]
 x = []      # Positions
 v = []      # Velocities
 f = []      # Forces
@@ -18,9 +18,9 @@ m = 1       # Mass
 w = 1       # Angular Frequency for spring
 f.append(get_force(x[0])) # Compute inital force
 
-for i in range(ngrid):
+for i in range(n_steps):
     x.append( x[i] + v[i] * dt + ((f[i])/(2 * m) * dt**2) )
     v_half = v[i] + (f[i]/(2 * m)) * dt
-    if i < ngrid:
+    if i < n_steps:
         f.append(get_force(x[i + 1]))
     v.append( v_half + (f[i + 1]/(2 * m)) * dt )
