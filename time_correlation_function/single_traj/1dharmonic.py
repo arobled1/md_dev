9c99b5a784ed77afa2e688299584cd65f95e3427
Alan Robledo <ar6138@nyu.edu>
2020-05-26 17:45:43 -0700

    added more comments

--- time_correlation_function/single_traj/1dharmonic.py
+++ time_correlation_function/single_traj/1dharmonic.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 def get_force(position): # F = - grad(U) = - grad(.5 k x^2) = - kx = - m w^2 x
     return - m * w**2 * position
 
-tmin = 0        # Starting time
-dt = 0.01       # Time step
-n_steps = 50000  # Number of time steps
+#============================================================================
+# This block is for setting up paramters
+tmin = 0                # Starting time
+dt = 0.01               # Time step
+n_steps = 50000         # Number of time steps
 
 times = np.array([tmin + i * dt for i in range(n_steps)])
 x = np.zeros((len(times)))      # Initialize Positions
@@ -18,6 +20,8 @@ m = 1                   # Set mass
 w = 2                   # Angular Frequency for spring
 f[0] = get_force(x[0])  # Compute inital force
 
+#============================================================================
+# This block is for running MD
 for i in range(1,n_steps):
     # Update position
     x[i] = x[i - 1] + v[i - 1] * dt + ((f[i - 1])/(2 * m) * dt**2)
@@ -28,6 +32,7 @@ for i in range(1,n_steps):
     # Update velocity
     v[i] = v_half + (f[i]/(2 * m)) * dt
 
+#============================================================================
 # This block is for computing velocity autocorrelation
 K = 500    # User specified number of points in each segment  k << M
 auto_times = [tmin + i * dt for i in range(K)]
@@ -43,8 +48,8 @@ for n in range(K):
 plt.plot(auto_times,v_auto, '-', color = "blue")
 plt.xlabel("t")
 plt.ylabel('Velocity Autocorrelation')
-plt.title("K = 150")
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
 plt.tight_layout()
 plt.savefig("autocorrelation.pdf")
 plt.clf()
+#============================================================================
