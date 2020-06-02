13ea25c65cef094c28a92f216d6dc71eee1e9abf
Alan Robledo <ar6138@nyu.edu>
2020-06-02 16:05:03 -0700

    added code to check histogram of velocities

--- time_correlation_function/mutliple_traj/get_samples.py
+++ time_correlation_function/mutliple_traj/get_samples.py
@@ -18,20 +18,28 @@ def rand_kick(old_v, friction, boltz, mass, deltat):
 def get_force(posit, mass, frequency):
     return - mass * frequency**2 * posit
 
-tmin = 0            # Starting time
-dt = 0.01           # Delta t
-n_steps = 30000     # Number of time steps
-kbt = 80
-gamma = 0.01
+def bin_centers(bin_edges):
+    return (bin_edges[1:]+bin_edges[:-1])/2.
+
+def get_harmonic_density(veloci, inv_temp, mass, omega):
+    normalization = (m / (2*np.pi * inv_temp))**0.5
+    exp_constant = mass / (2*inv_temp)
+    return normalization * np.exp(-exp_constant * veloci**2 )
+
+tmin = 0                # Starting time
+dt = 0.01               # Delta t
+n_steps = 250000        # Number of time steps
+kbt = 10
+gamma = 10
 
 times = np.array([tmin + i * dt for i in range(n_steps)])
 x = np.zeros((len(times)))      # Initialize Positions
 v = np.zeros((len(times)))      # Initialize Velocities
 f = np.zeros((len(times)))      # Initialize Forces
-m = 2                           # Set mass
-w = 3                           # Angular Frequency for spring
-x[0] = 0                        # Initial position
-v[0] = 0.1                      # Initial velocity
+m = 1                           # Set mass
+w = np.sqrt(80)                # Angular Frequency for spring
+x[0] = .1                        # Initial position
+v[0] = .5                        # Initial velocity
 f[0] = get_force(x[0], m, w)    # Compute inital force
 
 for i in range(1,n_steps):
@@ -48,7 +56,24 @@ for i in range(1,n_steps):
     # Update velocity based on random force
     v[i] = upd_velocity(v[i], f[i], dt, m)
 
-save_frequency = 500
+#============================================================================
+# This block is for computing a histogram of the velocities to check that the
+#   maxwell boltzmann distribution is produced.
+check = np.arange(-15, 15, 0.1)
+dist_hist, dist_bin_edges = np.histogram(v[-len(v)//2:],bins=20,density=True)
+ideal_prediction_x = get_harmonic_density(bin_centers(dist_bin_edges), kbt, m, w)
+p = plt.plot(bin_centers(dist_bin_edges), dist_hist,marker='o',label='P(v)',linestyle='')
+plt.plot(check, get_harmonic_density(check, kbt, m, w),linestyle='-',label='Ideal P(v)')
+plt.legend(loc='upper left')
+plt.xlim(-15, 15)
+plt.ylim(0,.14)
+plt.xlabel('v')
+plt.ylabel('P(v)')
+plt.savefig('harmonic_vel_dens.pdf')
+plt.clf()
+#============================================================================
+
+save_frequency = 50
 samples_x = x[::save_frequency]
 samples_x = np.delete(samples_x,0)
 samples_v = v[::save_frequency]
