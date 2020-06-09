9e29ee23a180f506c02c5c697dcf37870f4e448e
Alan Robledo <ar6138@nyu.edu>
2020-06-09 14:44:50 -0700

    got rid of numpy arrays

--- verlet/1dharmonic.py
+++ verlet/1dharmonic.py
@@ -1,36 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_force(position): # F = - grad(U) = - grad(.5 k x^2) = - kx = - m w^2 x
-    return - m * w**2 * position
+def get_force(position, mass, frequency): # F = - grad(U) = - grad(.5 k x^2) = - kx = - m w^2 x
+    return - mass * (frequency**2) * position
 
 tmin = 0        # Starting time
 dt = 0.01       # Time step
-n_steps = 500  # Number of time steps
+n_steps = 500   # Number of time steps
 
 times = np.array([tmin + i * dt for i in range(n_steps)])
-x = np.zeros((len(times)))      # Initialize Positions
-v = np.zeros((len(times)))      # Initialize Velocities
-f = np.zeros((len(times)))      # Initialize Forces
-x[0] = 0                # Initial position
-v[0] = 1                # Initial velocity
+x = 0                    # Initial position
+v = 1                   # Initial velocity
 m = 1                   # Set mass
 w = 2                   # Angular Frequency for spring
-f[0] = get_force(x[0])  # Compute inital force
-
+f = get_force(x, m, w)  # Compute inital force
+positions = [x]
+velocities = [v]
 for i in range(1,n_steps):
     # Update position
-    x[i] = x[i - 1] + v[i - 1] * dt + ((f[i - 1])/(2 * m) * dt**2)
+    x = x + v * dt + (f/(2 * m) * dt**2)
     # Compute velocity at half step
-    v_half = v[i - 1] + (f[i - 1]/(2 * m)) * dt
+    v_half = v + (f/(2 * m)) * dt
     # Update force
-    f[i] = get_force(x[i])
+    f = get_force(x, m, w)
     # Update velocity
-    v[i] = v_half + (f[i]/(2 * m)) * dt
+    v = v_half + (f/(2 * m)) * dt
+    # Keep position value
+    positions.append(x)
+    # Keep velocity value
+    velocities.append(v)
 
 plt.xlim(0, max(times))
-plt.ylim(-max(x) - .1,max(x) + .1)
-plt.plot(times, x, 'o', color='blue')
+plt.ylim(-max(positions) - .1,max(positions) + .1)
+plt.plot(times, positions, '-', color='blue')
 plt.xlabel("t")
 plt.ylabel("x(t)")
 plt.savefig("positions.pdf")
