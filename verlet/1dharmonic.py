5e0df496dd83377b2ea8fedb03dc148fd200c8d8
Alan Robledo <ar6138@nyu.edu>
2021-04-26 13:22:33 -0700

    added functions

--- verlet/1dharmonic.py
+++ verlet/1dharmonic.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 def get_force(position, mass, frequency): # F = - grad(U) = - grad(.5 k x^2) = - kx = - m w^2 x
     return - mass * (frequency**2) * position
 
+def upd_pos(old_pos, vel, time):
+    return old_pos + vel*time
+
+def upd_vel(old_vel, force, mass, time):
+    return old_vel + (force * time)/(2 * mass)
+
 tmin = 0        # Starting time
 dt = 0.01       # Time step
 n_steps = 500   # Number of time steps
 
 times = np.array([tmin + i * dt for i in range(n_steps)])
-x = 0                    # Initial position
+x = 0                   # Initial position
 v = 1                   # Initial velocity
 m = 1                   # Set mass
 w = 2                   # Angular Frequency for spring
@@ -18,13 +24,13 @@ positions = [x]
 velocities = [v]
 for i in range(1,n_steps):
     # Compute velocity at half step
-    v += (f/(2 * m)) * dt
+    v = upd_vel(v, f, m, dt)
     # Update position
-    x += v*dt
+    x = upd_pos(x, v, dt)
     # Update force
     f = get_force(x, m, w)
     # Update velocity
-    v += + (f/(2 * m)) * dt
+    v = upd_vel(v, f, m, dt)
     # Keep position value
     positions.append(x)
     # Keep velocity value
